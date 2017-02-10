fb82d7c100bb528ecfef1531934bd620fc77f55f
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-MacBook-Pro.local>
2017-02-10 08:27:24 -0500

    initial file

---
+++ testt.py
@@ -0,0 +1,10 @@
+# import nltk
+
+mystring = "Monty Python ! And the holy grail ! \n"
+
+# No argument - treat space as delimiter
+print(mystring.split())
+
+print(mystring.strip())
+print(mystring.upper())
+print(mystring.lower())
