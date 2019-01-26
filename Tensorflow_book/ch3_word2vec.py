9e2a9415a61a452f7f371376ef61acc60079e802
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-01-26 21:13:35 +0300

    Updated Natural-Language-Processing-with-TensorFlow.

--- Tensorflow_book/ch3_word2vec.py
+++ Tensorflow_book/ch3_word2vec.py
@@ -72,51 +72,51 @@ data_index = 0
 
 
 def generate_batch_skip_gram(batch_size, window_size):
-  # data_index is updated by 1 everytime we read a data point
-  global data_index
+# data_index is updated by 1 everytime we read a data point
+global data_index
 
-  # two numpy arras to hold target words (batch)
-  # and context words (labels)
-  batch = np.ndarray(shape=(batch_size), dtype=np.int32)
-  labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+# two numpy arras to hold target words (batch)
+# and context words (labels)
+batch = np.ndarray(shape=(batch_size), dtype=np.int32)
+labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
 
-  # span defines the total window size, where
-  # data we consider at an instance looks as follows.
-  # [ skip_window target skip_window ]
-  span = 2 * window_size + 1
+# span defines the total window size, where
+# data we consider at an instance looks as follows.
+# [ skip_window target skip_window ]
+span = 2 * window_size + 1
 
-  # The buffer holds the data contained within the span
-  buffer = collections.deque(maxlen=span)
+# The buffer holds the data contained within the span
+buffer = collections.deque(maxlen=span)
 
-  # Fill the buffer and update the data_index
-  for _ in range(span):
-    buffer.append(data[data_index])
-    data_index = (data_index + 1) % len(data)
+# Fill the buffer and update the data_index
+for _ in range(span):
+buffer.append(data[data_index])
+data_index = (data_index + 1) % len(data)
 
-  # This is the number of context words we sample for a single target word
-  num_samples = 2*window_size
+# This is the number of context words we sample for a single target word
+num_samples = 2*window_size
 
-  # We break the batch reading into two for loops
-  # The inner for loop fills in the batch and labels with
-  # num_samples data points using data contained withing the span
-  # The outper for loop repeat this for batch_size//num_samples times
-  # to produce a full batch
-  for i in range(batch_size // num_samples):
-    k=0
-    # avoid the target word itself as a prediction
-    # fill in batch and label numpy arrays
-    for j in list(range(window_size))+list(range(window_size+1,2*window_size+1)):
-      batch[i * num_samples + k] = buffer[window_size]
-      labels[i * num_samples + k, 0] = buffer[j]
-      k += 1
+# We break the batch reading into two for loops
+# The inner for loop fills in the batch and labels with
+# num_samples data points using data contained withing the span
+# The outper for loop repeat this for batch_size//num_samples times
+# to produce a full batch
+for i in range(batch_size // num_samples):
+k=0
+# avoid the target word itself as a prediction
+# fill in batch and label numpy arrays
+for j in list(range(window_size))+list(range(window_size+1,2*window_size+1)):
+batch[i * num_samples + k] = buffer[window_size]
+labels[i * num_samples + k, 0] = buffer[j]
+k += 1
 
-    # Everytime we read num_samples data points,
-    # we have created the maximum number of datapoints possible
-    # withing a single span, so we need to move the span by 1
-    # to create a fresh new span
-    buffer.append(data[data_index])
-    data_index = (data_index + 1) % len(data)
-  return batch, labels
+# Everytime we read num_samples data points,
+# we have created the maximum number of datapoints possible
+# withing a single span, so we need to move the span by 1
+# to create a fresh new span
+buffer.append(data[data_index])
+data_index = (data_index + 1) % len(data)
+return batch, labels
 
 print('data:', [reverse_dictionary[di] for di in data[:8]])
 
