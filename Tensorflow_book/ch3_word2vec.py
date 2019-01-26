e15483d7430f6a2cb6d09cd78f61bec1240e28ba
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-01-26 17:59:58 +0300

    Updated Natural-Language-Processing-with-TensorFlow.

---
+++ Tensorflow_book/ch3_word2vec.py
@@ -0,0 +1,17 @@
+import collections
+import math
+import numpy as np
+import os
+import random
+import tensorflow as tf
+import bz2
+from matplotlib import pylab
+from six.moves import range
+from six.moves.urllib.request import urlretrieve
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+import nltk # standard preprocessing
+import operator # sorting items in dictionary by value
+#nltk.download() #tokenizers/punkt/PY3/english.pickle
+from math import ceil
+import csv
