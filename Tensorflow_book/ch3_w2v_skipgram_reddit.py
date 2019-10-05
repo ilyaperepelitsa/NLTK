cfe1333d01e063501eaa73dfe670f3acf9f3037e
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-05 16:53:43 +0300

    Updated Natural-Language-Processing-with-TensorFlow.

--- Tensorflow_book/ch3_w2v_skipgram_reddit.py
+++ Tensorflow_book/ch3_w2v_skipgram_reddit.py
@@ -50,7 +50,7 @@ DATA.head()
 
 DATA = pd.concat([DATA['text'], DATA["parent_text"]], axis = 0)
 DATA
-text = DATA['Dialogue'].str.cat(sep = ". ")
+text = DATA.str.cat(sep = ". ")
 # len(text)
 # text
 
