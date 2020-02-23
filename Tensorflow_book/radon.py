0b82da00164a2b7198c54ac347d26f3806bd1663
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2020-02-23 15:28:26 +0300

    Updated Natural-Language-Processing-with-TensorFlow.

--- Tensorflow_book/radon.py
+++ Tensorflow_book/radon.py
@@ -1,14 +1,14 @@
-text = "I evah neeb dnuora rof etiuq a rebmun fo sraey won; I t'now yas woh ynam. Tub hguorht eht sedaced I evah deciton taht ereht era sgniht dna secalp dna saedi taht evah nesir evoba eht mortsleam fo yadyreve gnivil ni eht ytnewt-tsrif yrutnec.Eseht sdnarb, secalp, stcejbo dna os no, mees ot evah a neehs taht si erom emilbus naht rieht sreep. Yeht dnats tuo esuaceb fo rieht ytilauq, rieht erulla ro ylpmis esuaceb fo rieht ytiliba ot dnats tuo morf eht dworc.Ereht si a ytilanommoc ot eseht snoci fo elyts, tseretni dna erised. Rof emos nosaer yeht lla mees ot nigeb htiw eht rettel B"
-
-
-text
-" ".join([i[::-1] for i in text.split()])
-
-
-I have been around for quite a number of years now;
- I won't say how many. buT through the decades I have noticed that there are things
-  and places and ideas that have risen above the maelstrom of everyday living in the first-twenty
-   century. thesE ,brands ,places objects and so on, seem to have a sheen that
-   is more sublime than their peers.
-    theY stand out because of their ,quality their allure or simply because of their ability
-    to stand out from the therE.crowd is a commonality to these icons of ,style interest and .desire foR some reason they all seem to begin with the letter B"
+# text = "I evah neeb dnuora rof etiuq a rebmun fo sraey won; I t'now yas woh ynam. Tub hguorht eht sedaced I evah deciton taht ereht era sgniht dna secalp dna saedi taht evah nesir evoba eht mortsleam fo yadyreve gnivil ni eht ytnewt-tsrif yrutnec.Eseht sdnarb, secalp, stcejbo dna os no, mees ot evah a neehs taht si erom emilbus naht rieht sreep. Yeht dnats tuo esuaceb fo rieht ytilauq, rieht erulla ro ylpmis esuaceb fo rieht ytiliba ot dnats tuo morf eht dworc.Ereht si a ytilanommoc ot eseht snoci fo elyts, tseretni dna erised. Rof emos nosaer yeht lla mees ot nigeb htiw eht rettel B"
+#
+#
+# text
+# " ".join([i[::-1] for i in text.split()])
+#
+#
+# I have been around for quite a number of years now;
+#  I won't say how many. buT through the decades I have noticed that there are things
+#   and places and ideas that have risen above the maelstrom of everyday living in the first-twenty
+#    century. thesE ,brands ,places objects and so on, seem to have a sheen that
+#    is more sublime than their peers.
+#     theY stand out because of their ,quality their allure or simply because of their ability
+#     to stand out from the therE.crowd is a commonality to these icons of ,style interest and .desire foR some reason they all seem to begin with the letter B"
